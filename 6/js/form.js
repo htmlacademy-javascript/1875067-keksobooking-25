bf3094d49035622071c20da32757df642ce2d763
Keks <keks@htmlacademy.ru>
2022-04-15 20:12:27 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/form.js
+++ 6/js/form.js
@@ -31,57 +31,53 @@ const roomsOptions = {
   '100': ['0'],
 };
 
-function validateTitle (value) {
-  return value.length >= 30 && value.length <= 100;
-}
+const validateTitle = (value) => value.length >= 30 && value.length <= 100;
 
 pristine.addValidator(title, validateTitle, 'Заголовок должен быть от 30 до 100 символов длиной');
 
 
-function validatePrice (value) {
-  return value.length && parseInt(value, 10) >= minPrice[type.value] && parseInt(value, 10) <= maxPrice;
-}
+const validatePrice = (value) => value.length && parseInt(value, 10) >= minPrice[type.value] && parseInt(value, 10) <= maxPrice;
 
-function getPriceErrorMessage (value) {
+const getPriceErrorMessage = (value) => {
   if (parseInt(value, 10) > maxPrice) {
     return `Максимальная стоимость - ${maxPrice} руб.`;
   }
   return `Минимальная стоимость ${minPrice[type.value]} руб`;
-}
+};
 
 pristine.addValidator(priceField, validatePrice, getPriceErrorMessage);
 
-function onTypeChange () {
-  priceField.placeholder = minPrice[this.value];
+const onTypeChange = () => {
+  priceField.placeholder = minPrice[type.value];
   pristine.validate(priceField);
-}
+};
 
 form.querySelectorAll('[name="type"]').forEach((item) => item.addEventListener('change', onTypeChange));
 
 
-function validateGuests () {
-  return roomsOptions[roomsField.value].includes(guestsField.value);
-}
+const validateGuests = () => roomsOptions[roomsField.value].includes(guestsField.value);
 
-function getGuestsErrorMessage () {
+const getGuestsErrorMessage = () => {
   switch (roomsField.value) {
     case '1': return 'Не больше 1 гостя';
     case '2': return 'Не больше 2 гостей';
     case '3': return 'Не больше 3 гостей';
     case '100': return 'Не для гостей';
   }
-}
+};
 
 pristine.addValidator(guestsField, validateGuests, getGuestsErrorMessage);
 
-function onRoomsChange () {
-  pristine.validate(guestsField);
-}
+const onRoomsChange = () => pristine.validate(guestsField);
 
 form.querySelectorAll('[name="rooms"]').forEach((item) => item.addEventListener('change', onRoomsChange));
 
 
 form.addEventListener('submit', (evt) => {
-  evt.preventDefault();
   pristine.validate();
+  const isValid = pristine.validate();
+  if (!isValid) {
+    evt.preventDefault();
+  }
 });
+
